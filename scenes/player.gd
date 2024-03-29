c5ac0b5fc90f26bd80d006747bfdd67c930582a4
tinSegur <martin.segur@ug.uchile.cl>
2024-03-29 01:13:24 -0300

    add: inputs para movimiento del jugador, modificaciones a la escena de jugador

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,8 +1,14 @@
 class_name Player
 extends CharacterBody2D
 
+var gravity = 0.98
+var jump_speed = 0.2
+var speed = 0.15
+var acceleration = 0.1
+
 @onready var multiplayer_spawner: MultiplayerSpawner = $MultiplayerSpawner
 @onready var multiplayer_synchronizer: MultiplayerSynchronizer = $MultiplayerSynchronizer
+
 
 @export var bullet_scene: PackedScene
 
@@ -22,6 +28,22 @@ func _input(event: InputEvent) -> void:
 			# triggers syncronizer
 			score += 1
 
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		
+	if is_multiplayer_authority():
+		if Input.is_action_just_pressed("jump"):
+			test.rpc("jump!")
+			velocity.y = -jump_speed
+		
+		var move_input = Input.get_axis("move_left", "move_right")
+		velocity.x = move_toward(velocity.x, move_input * speed, acceleration * delta)
+		#send_data.rpc(global_position, velocity)
+	move_and_slide()
+
+
+
 func setup(player_data: Statics.PlayerData):
 	name = str(player_data.id)
 	set_multiplayer_authority(player_data.id)
@@ -35,3 +57,8 @@ func test(name):
 	var sender_player = Game.get_player(sender_id)
 	Debug.sprint(message)
 	Debug.sprint(sender_player.name)
+
+@rpc
+func send_data(pos : Vector2, vel : Vector2):
+	global_position = lerp(global_position, pos, 0.75)
+	velocity = lerp(velocity, vel, 0.75)
