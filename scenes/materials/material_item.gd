dc1566d0bc6fb64673a4abe793f4c62b379fd5f2
diego <diego@terminus.cl>
2024-05-01 17:31:50 -0400

    fix pipe corner on axis

--- scenes/materials/material_item.gd
+++ scenes/materials/material_item.gd
@@ -28,7 +28,6 @@ func ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#tilemap = get_tree().current_scene.get_node("TileMap")
 	var tile_coords = tilemap.get_tile_coords(global_position)
 	var tile: TileData= tilemap.get_cell_tile_data(3, tile_coords)
 	if is_instance_valid(tile):
@@ -63,11 +62,14 @@ func _physics_process(delta: float) -> void:
 		if speed == 0:
 			return
 		
+		#var build_pos = Vector2i(mouse_pos.x - mouse_pos.x%18 + 9 * (1 if (sign(mouse_pos.x) == 0) else sign(mouse_pos.x)), 
+			 					#mouse_pos.y - mouse_pos.y%18 + 2)
+		
 		if dir.y == 0:
-			if int(global_position.y)%18 < 9:
-				global_position.y += speed
-			elif int(global_position.y)%18 > 9:
-				global_position.y -= speed
+			if abs(int(global_position.y)%18) < 9:
+				global_position.y += speed * (1 if (sign(global_position.y) == 0) else sign(global_position.y))
+			elif abs(int(global_position.y)%18) > 9:
+				global_position.y -= speed * sign(global_position.y)
 			else:
 				var next_pipe = tilemap.get_cell_tile_data(0, tilemap.get_tile_coords(global_position + dir*6))
 				if is_instance_valid(next_pipe):
@@ -75,10 +77,10 @@ func _physics_process(delta: float) -> void:
 					if ((!next_pipe.get_custom_data("occupied") and next_pipe.get_custom_data("pipe_speed")!= 0) or next_coords == pipe_coords):
 						global_position += dir*speed
 		else:
-			if int(global_position.x)%18 < 9:
-				global_position.x += speed
-			elif int(global_position.x)%18 > 9:
-				global_position.x -= speed
+			if int(abs(global_position.x))%18 < 9:
+				global_position.x += speed * (1 if (sign(global_position.x) == 0) else sign(global_position.x))
+			elif int(abs(global_position.x))%18 > 9:
+				global_position.x -= speed * sign(global_position.x)
 			else:
 				var next_pipe = tilemap.get_cell_tile_data(0, tilemap.get_tile_coords(global_position + dir*6))
 				if is_instance_valid(next_pipe):
