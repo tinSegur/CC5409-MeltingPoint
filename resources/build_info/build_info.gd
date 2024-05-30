7888cf9933515cb43bfff87d7b61be8075c8f793
diego <diego@terminus.cl>
2024-05-30 18:48:24 -0400

    update build menu

---
+++ resources/build_info/build_info.gd
@@ -0,0 +1,41 @@
+class_name MPBuildInfo
+extends Resource
+
+@export var name : String
+@export var icon: Texture2D
+@export var description : String
+@export var costs : Array[MPCost]
+
+func render_info(parent_node: VBoxContainer):
+	var name_label : Label = Label.new()
+	name_label.text = name
+	name_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	parent_node.add_child(name_label)
+	
+	var image : TextureRect = TextureRect.new()
+	image.texture = icon
+	image.expand_mode = TextureRect.EXPAND_KEEP_SIZE
+	image.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+	image.custom_minimum_size = Vector2(48,48)
+	parent_node.add_child(image)
+	
+	var desc_label : RichTextLabel = RichTextLabel.new()
+	desc_label.text = description
+	desc_label.fit_content = true
+	desc_label["theme_override_font_sizes/normal_font_size"] = 8
+	parent_node.add_child(desc_label)
+	
+	var cost_grid : GridContainer = GridContainer.new()
+	cost_grid.columns = 4
+	parent_node.add_child(cost_grid)
+	
+	for cost in costs:
+		var cost_icon : TextureRect = TextureRect.new()
+		cost_icon.texture = cost.icon
+		cost_icon.custom_minimum_size = Vector2(16,16)
+		cost_grid.add_child(cost_icon)
+		
+		var cost_label : Label = Label.new()
+		cost_label.text = str(cost.amount)
+		cost_label["theme_override_font_sizes/font_size"] = 8
+		cost_grid.add_child(cost_label)
--- scenes/ui/build_menu.gd
+++ scenes/ui/build_menu.gd
@@ -5,32 +5,68 @@ var player: Player
 @onready var platform_button = $PanelContainer/MarginContainer/HBoxContainer/MachineSelector/GridContainer/PlatformButton
 @onready var pipe_button = $PanelContainer/MarginContainer/HBoxContainer/MachineSelector/GridContainer/PipeButton
 @onready var ext_button = $PanelContainer/MarginContainer/HBoxContainer/MachineSelector/GridContainer/OutputButton
+@onready var machine_preview = $PanelContainer/MarginContainer/HBoxContainer/MachinePreview
+@onready var buttons_container = $PanelContainer/MarginContainer/HBoxContainer/MachineSelector/GridContainer
+
+var build_info : MPBuildInfo
 
 signal machine_selected
 signal tile_selected
 
 func _ready():
 	player = get_parent().get_parent()
-	miner_button.pressed.connect(_miner_selected)
-	platform_button.pressed.connect(_platform_selected)
-	pipe_button.pressed.connect(_pipe_selected)
-	ext_button.pressed.connect(_extractor_selected)
 	
+	miner_button.pressed.connect(_miner_selected)
+	miner_button.mouse_entered.connect(_miner_hover)
+	
+	platform_button.pressed.connect(_platform_selected)
+	platform_button.mouse_entered.connect(_platform_hover)
+	
+	pipe_button.pressed.connect(_pipe_selected)
+	pipe_button.mouse_entered.connect(_pipe_hover)
+	
+	ext_button.pressed.connect(_extractor_selected)
+	ext_button.mouse_entered.connect(_extractor_hover)
+	
+	for button in buttons_container.get_children():
+		button.mouse_exited.connect(_clear_info)
+	
+
+func _clear_info():
+	for node in machine_preview.get_children():
+		node.queue_free()
+
+func _miner_hover():
+	build_info = load("res://resources/build_info/miner_info.tres")
+	build_info.render_info(machine_preview)
 
 func _miner_selected():
 	player.build_scene = "res://scenes/machines/miner.tscn"
+	
 	machine_selected.emit()
 	visible = false
+
+func _platform_hover():
+	build_info = load("res://resources/build_info/platform_info.tres")
+	build_info.render_info(machine_preview)
 
 func _platform_selected():
 	player.tile_index = 0
 	tile_selected.emit()
 	visible = false
 
+func _pipe_hover():
+	build_info = load("res://resources/build_info/pipe_info.tres")
+	build_info.render_info(machine_preview)
+
 func _pipe_selected():
 	player.tile_index = 1
 	tile_selected.emit()
 	visible = false
+
+func _extractor_hover():
+	build_info = load("res://resources/build_info/extractor_info.tres")
+	build_info.render_info(machine_preview)
 
 func _extractor_selected():
 	player.build_scene = "res://scenes/machines/hub_output.tscn"
