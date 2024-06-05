eb0a575e1f5a939aaa34875ccca4d05d70dc26e4
elvie@babel <martin.segur@ug.uchile.cl>
2024-06-05 15:03:39 -0400

    cosas

--- scenes/machines/cutter.gd
+++ scenes/machines/cutter.gd
@@ -2,8 +2,12 @@ extends Machine
 
 @onready var internal_inv : InternalInventory = $InternalInventory
 
+
+
+
 func input_resource(item : MPMaterial, liquid : bool):
 	if liquid:
 		internal_inv.add_stock(item.id, 1, Statics.Material_states.LIQUID)
 	else:
-		internal_inv.add_stock(item.id, 1, )
+		internal_inv.add_stock(item.id, 1, Statics.Material_states.SOLID)
+
