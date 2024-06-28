7f6c8f82f366d88d29248f24bd7056f3ba1e6e37
diego <diego@terminus.cl>
2024-06-28 17:35:08 -0400

    update main menu

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,16 +1,30 @@
 extends Control
 
-@onready var play_button = $VBoxContainer/NinePatchRect/VBoxContainer/PlayButton
-@onready var help_button = $VBoxContainer/NinePatchRect/VBoxContainer/HelpButton
-@onready var credits_button = $VBoxContainer/NinePatchRect/VBoxContainer/CreditsButton
-@onready var quit_button = $VBoxContainer/NinePatchRect/VBoxContainer/QuitButton
+@onready var play_button = %PlayButton
+@onready var help_button = %HelpButton
+@onready var credits_button = %CreditsButton
+@onready var quit_button = %QuitButton
+@onready var credits_back_button = %CreditsBackButton
+
+@onready var menu_container = %MenuContainer
+@onready var credits = %Credits
 
 func _ready():
-	play_button.pressed.connect(on_play_pressed)
-	quit_button.pressed.connect(on_quit_pressed)
+	play_button.pressed.connect(_on_play_pressed)
+	credits_button.pressed.connect(_on_credits_pressed)
+	credits_back_button.pressed.connect(_on_credits_back_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
 
-func on_play_pressed():
+func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/lobby.tscn")
 
-func on_quit_pressed():
+func _on_quit_pressed():
 	get_tree().quit()
+	
+func _on_credits_pressed():
+	menu_container.visible = false
+	credits.visible = true
+	
+func _on_credits_back_pressed():
+	credits.visible = false
+	menu_container.visible = true
